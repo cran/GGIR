fd9033d0cbf2704c4a4ab1dd5839f6043dead96d
Francisco Javier Barón López <fjbaron@gmail.com>
2017-06-04 01:41:37 +0200

    Update g.part5.R
    Control de errores

--- R/g.part5.R
+++ R/g.part5.R
@@ -110,7 +110,14 @@ g.part5 = function(datadir=c(),metadatadir=c(),f0=c(),f1=c(),strategy=1,maxdur=7
       }
       # load output g.part2
       selp = which(fnames.ms2 == fnames.ms3[i]) # so, fnames.ms3[i] is the reference point for filenames
+       try({
+        hayError=TRUE
       load(file=paste(metadatadir,"/meta/ms2.out/",fnames.ms2[selp],sep=""))
+      hayError=FALSE
+      })
+      if(hayError){
+        stop(paste(metadatadir,"/meta/ms2.out/",fnames.ms2[selp],sep=""))
+      }
       daysummary = SUM$daysummary
       summary = SUM$summary
       # load output g.part4
