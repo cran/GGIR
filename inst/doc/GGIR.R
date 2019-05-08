507fcd6b7d4566a931442df2eb73edfc5c305860
Vincent T van Hees <vincentvanhees@gmail.com>
2019-05-08 16:20:09 +0000

    version 1.9-1

--- inst/doc/GGIR.R
+++ inst/doc/GGIR.R
@@ -6,72 +6,35 @@ knitr::opts_chunk$set(echo = TRUE)
 
 ## ----eval=FALSE----------------------------------------------------------
 #  library(GGIR)
-#  g.shell.GGIR(#=======================================
-#               # INPUT NEEDED:
-#               mode=c(1,2,3,4,5),
+#  g.shell.GGIR(datadir="C:/mystudy/mydata",
+#               outputdir="D:/myresults")
+
+## ---- out.width = "700px",echo=FALSE-------------------------------------
+knitr::include_graphics("sleeplogexample.jpg")
+
+## ----eval=FALSE----------------------------------------------------------
+#  library(GGIR)
+#  g.shell.GGIR(mode=c(1,2,3,4,5),
 #               datadir="C:/mystudy/mydata",
 #               outputdir="D:/myresults",
-#               f0=1, f1=2,
-#               #-------------------------------
-#               # Part 1:
-#               #-------------------------------
-#               # Key functions: reading file, auto-calibration, and extracting features
-#               do.enmo = TRUE,             do.anglez=TRUE,
-#               chunksize=1,                printsummary=TRUE,
-#               #-------------------------------
-#               # Part 2:
-#               #-------------------------------
-#               strategy = 2,               ndayswindow=7,
+#               strategy = 1,
 #               hrs.del.start = 0,          hrs.del.end = 0,
 #               maxdur = 9,                 includedaycrit = 16,
-#               winhr = c(5,10),
-#               qlevels = c(c(1380/1440),c(1410/1440)),
 #               qwindow=c(0,24),
-#               ilevels = c(seq(0,400,by=50),8000),
-#               mvpathreshold =c(100,120),
+#               mvpathreshold =c(100),
 #               bout.metric = 4,
-#               closedbout=FALSE,
-#               #-------------------------------
-#               # Part 3:
-#               #-------------------------------
-#               # Key functions: Sleep detection
-#               timethreshold= c(5),        anglethreshold=5,
-#               ignorenonwear = TRUE,
-#               #-------------------------------
-#               # Part 4:
-#               #-------------------------------
-#               # Key functions: Integrating sleep log (if available) with sleep detection
-#               # storing day and person specific summaries of sleep
-#               excludefirstlast = TRUE,
+#               excludefirstlast = FALSE,
 #               includenightcrit = 16,
-#               def.noc.sleep = c(),
-#               loglocation= "C:/mydata/sleeplog.csv",
+#               def.noc.sleep = c(1),
 #               outliers.only = TRUE,
 #               criterror = 4,
-#               relyonsleeplog = FALSE,
-#               sleeplogidnum = TRUE,
-#               colid=1,
-#               coln1=2,
 #               do.visual = TRUE,
-#               nnights = 9,
-#               #-------------------------------
-#               # Part 5:
-#               # Key functions: Merging physical activity with sleep analyses
-#               #-------------------------------
 #               threshold.lig = c(30), threshold.mod = c(100),  threshold.vig = c(400),
 #               boutcriter = 0.8,      boutcriter.in = 0.9,     boutcriter.lig = 0.8,
 #               boutcriter.mvpa = 0.8, boutdur.in = c(1,10,30), boutdur.lig = c(1,10),
 #               boutdur.mvpa = c(1),   timewindow = c("WW"),
-#               #-----------------------------------
-#               # Report generation
-#               #-------------------------------
-#               # Key functions: Generating reports based on meta-data
 #               do.report=c(2,4,5),
-#               visualreport=TRUE,     dofirstpage = TRUE,
-#               viewingwindow=1)
-
-## ---- out.width = "700px",echo=FALSE-------------------------------------
-knitr::include_graphics("sleeplogexample.jpg")
+#               visualreport=TRUE)
 
 ## ----eval=FALSE----------------------------------------------------------
 #  options(echo=TRUE)
