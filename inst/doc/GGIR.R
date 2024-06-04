9c1d76e13252785c5829eea20b623522d31ff89b
Vincent T van Hees <v.vanhees@accelting.com>
2024-06-04 16:00:10 +0000

    version 3.1-1

--- inst/doc/GGIR.R
+++ inst/doc/GGIR.R
@@ -86,9 +86,6 @@ knitr::include_graphics("reportexample.jpg")
 ## ----out.width = "700px",echo=FALSE-------------------------------------------
 knitr::include_graphics("example_dovisual.jpg")
 
-## ----out.width = "400px",echo=FALSE-------------------------------------------
-knitr::include_graphics("nonwearimage.jpg")
-
 ## ----echo=FALSE, out.width = "60%", out.extra='style="border: 0; padding:20px"'----
 knitr::include_graphics("GGIR-MASTERLOGO-RGB.png")
 
